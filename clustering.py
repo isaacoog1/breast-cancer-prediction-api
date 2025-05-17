6f91ad1de6ca4b3255befd0f1add0841e03cd21c
isaacoog1 <oguntuyokoredeshile@gmail.com>
2025-05-17 09:45:11 +0100

    Update

--- clustering.py
+++ clustering.py
@@ -1,7 +1,4 @@
-import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.datasets import load_breast_cancer
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
